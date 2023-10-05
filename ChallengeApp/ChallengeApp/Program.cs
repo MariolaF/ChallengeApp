c42cddf23079c0f4f6f2e71f52ddbc2bae4653c9
Mariola Furdak <furdakmariola77@gmail.com>
2023-10-05 19:58:31 +0100

    Zadanie domowe- Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string gender = "female";
+int age = 33;
+
+if (gender == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+else if (gender != "female" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (gender != "female" && age > 30)
+{
+    Console.WriteLine("Mężczyzna powyżej 30 lat");
+}
