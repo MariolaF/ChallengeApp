81c4db28ad64fa1ae63aa587a14013187b233035
Mariola Furdak <furdakmariola77@gmail.com>
2023-11-07 23:24:43 +0000

    Zadanie Domowe - Dzień 9

---
+++ ChallengeApp/ChallengeApp.Tests/Employee.Tests.cs
@@ -0,0 +1,48 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class Employee_Tests
+    {
+        [Test]
+        public void TestOfStatisticMaxValue()
+        {
+            var employee = new Employee("Mariola", "Furdak");
+            //arrange
+            employee.AddGrade(9);
+            employee.AddGrade(-5);
+            employee.AddGrade(8);
+            employee.AddGrade(2);
+            //act
+            var statistic = employee.GetStatistics();
+            //assert
+            Assert.AreEqual(9, statistic.Max);
+        }
+        [Test]
+        public void TestOfStatisticMinValue()
+        {
+            var employee = new Employee("Mariola", "Furdak");
+            //arrange
+            employee.AddGrade(9);
+            employee.AddGrade(-5);
+            employee.AddGrade(8);
+            employee.AddGrade(2);
+            //act
+            var statistic = employee.GetStatistics();
+            //assert
+            Assert.AreEqual(-5, statistic.Min);
+        }
+        [Test]
+        public void TestOfStatisticAverage()
+        {
+            var employee = new Employee("Mariola", "Furdak");
+            //arrange
+            employee.AddGrade(9);
+            employee.AddGrade(-5);
+            employee.AddGrade(8);
+            employee.AddGrade(2);
+            //act
+            var statistic = employee.GetStatistics();
+            //assert
+            Assert.AreEqual(3.5, statistic.Average);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,53 +1,12 @@
 ﻿using ChallengeApp;
 
-Employee employee1 = new("Daniel", "Kowalski", 28);
-Employee employee2 = new("Mariola", "Nowak", 22);
-Employee employee3 = new("Kamil", "Furdak", 30);
+var employee = new Employee("Mariola","Furdak");
 
-employee1.AddScore(2);
-employee1.AddScore(1);
-employee1.AddScore(5);
-employee1.AddScore(7);
-employee1.AddScore(9);
-//24
-employee2.AddScore(9);
-employee2.AddScore(5);
-employee2.AddScore(8);
-employee2.AddScore(2);
-employee2.AddScore(6);
-//30
-employee3.AddScore(8);
-employee3.AddScore(7);
-employee3.AddScore(8);
-employee3.AddScore(9);
-employee3.AddScore(9);
-//41
-
-List<Employee> employees = new()
-{
-    employee1, employee2, employee3
-};
-
-int maxResult = -1;
-Employee employeeWithMaxResult = null;
-
-foreach(var employee in employees)
-{
-    if(employee.Result > maxResult)
-    {
-        maxResult = employee.Result;
-        employeeWithMaxResult = employee;
-    }
-}
-
-Console.WriteLine("Najwyższą liczbe punktów: "
-    + maxResult
-    + " Uzyskał pracownik: "
-    + employeeWithMaxResult.Name
-    + " "
-    + employeeWithMaxResult.Surname
-    +" w wieku "
-    + employeeWithMaxResult.Age
-    +" lat.");
-
+employee.AddGrade(2);
+employee.AddGrade(2);
+employee.AddGrade(6);
+var statistic = employee.GetStatistics();
+Console.WriteLine($"Average: {statistic.Average:N2}");
+Console.WriteLine($"Min: {statistic.Min}");
+Console.WriteLine($"Max: {statistic.Max}");
 
