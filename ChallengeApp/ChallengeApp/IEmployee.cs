664dd268d22c63c83aaaed237a9b3dbcb9e4fc33
Mariola Furdak <furdakmariola77@gmail.com>
2024-02-13 10:48:53 +0000

    Zadanie domowe - 16

--- ChallengeApp/ChallengeApp/IEmployee.cs
+++ ChallengeApp/ChallengeApp/IEmployee.cs
@@ -5,9 +5,7 @@
         string Name { get; }
         string Surname { get; }
         void AddGrade(float grade);
-        void AddGrade(long grade);
         void AddGrade(double grade);
-        void AddGrade(decimal grade);
         void AddGrade(char grade);
         void AddGrade(int grade);
         void AddGrade(string grade);
